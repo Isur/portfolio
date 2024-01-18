8062fe0d0a3fb5fd3198205e2a23c0bdcfd8efac
Isur <bednarczykartur96@gmail.com>
2024-01-18 18:06:44 +0100

    Page name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Portfolio Artur "Isur" Bednarczyk',
+  title: "isur.dev",
   description: 'Portfolio Artur "Isur" Bednarczyk',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html className="scroll-smooth no-scrollbar bg-background text-primary" lang="en">
+    <html
+      className="scroll-smooth no-scrollbar bg-background text-primary"
+      lang="en"
+    >
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
