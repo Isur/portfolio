86eff66977353c7a5831b082250bbdfbeb2f66b5
Isur <bednarczykartur96@gmail.com>
2024-01-27 17:02:17 +0100

    Footer with shadow

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const Footer: FC = () => {
   const date = new Date().getFullYear();
   return (
-    <footer className="flex flex-col items-center justify-center h-16">
+    <footer className="flex flex-col items-center justify-center h-16 shadow-xd">
       <p>© {date} Artur Bednarczyk</p>
     </footer>
   );
