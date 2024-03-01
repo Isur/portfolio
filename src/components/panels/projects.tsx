aed2cc9b19a4e1121e6497ba4ca49189190c29c6
Isur <bednarczykartur96@gmail.com>
2024-03-01 22:49:29 +0100

    Projects content updates

--- src/components/panels/projects.tsx
+++ src/components/panels/projects.tsx
@@ -32,7 +32,7 @@ const PROJECTS: ProjectItemProps[] = [
   {
     title: "Overwrite.ai",
     description:
-      "Web application that utilizes AI to automatically generate descriptions for real estate properties. User interface let user upload images of the property, fill out a dynamic questionnaire, and provide profile details. Based on that data a unique property description is created. Subscription system, image enhancing, image size optimization, organization management, and more features were implemented.",
+      "A web application that harnesses AI to automatically generate descriptions for real estate properties. The user interface allows users to upload property images, complete a dynamic questionnaire, and provide profile details. Using this data, a unique property description is generated. Additionally, the application includes a subscription system, image enhancement, image size optimization, organization management, and more features.",
     dateRange: "2019 - 2021",
     techStack: ["TypeScript", "React", "Node.js", "PostgreSQL", "Python"],
     roleInProject: "Tech Leader, Fullstack Developer",
@@ -41,16 +41,25 @@ const PROJECTS: ProjectItemProps[] = [
   {
     title: "Pharmaspectre",
     description:
-      "Dashboards, colleting data from multiple sources. Data analysis and visualization. Medical documents search, preview and processing.",
+      "A platform that features dashboards that collect data from various sources, facilitating comprehensive data analysis and visualization. Additionally, it enables medical document search, preview, and processing functionalities.",
     dateRange: "2021",
     techStack: ["React", "Node.js", "PostgreSQL"],
     roleInProject: "Fullstack Developer",
     company: "ALM Services Technology Group",
   },
   {
+    title: "LegalSpot",
+    description:
+      "This web application is tailored for lawyers, offering comprehensive task, document, event, and register management, among other features. The system architecture prioritizes safety and security, with each company allocated its own database and instance of services, ensuring complete data separation. The application supports lawyers' workflows by facilitating task tracking and document generation based on templates created within a custom editor.",
+    dateRange: "2022 - 2024",
+    techStack: ["Python", "TypeScript", "Next.js", "PostgreSQL"],
+    roleInProject: "Architect, Team Leader, Backend Developer",
+    company: "Estimity",
+  },
+  {
     title: "Flux",
     description:
-      "Synchronization of LED strips with sound and video coming from different sources. Controlling the player and LED system was available via mobile app and whole system can be monitored from app with live stream from the camera connected to the control unit. Sensors were used to have information about temerature, humidity and oxygen. The system was designed to be used in medical facilities to work in faradays cage. Managing themes, music, videos, licences and users was accessible via web application.",
+      "The system synchronizes LED strips with audio and video streams from various sources. Control over the player and LED system is facilitated through a mobile app, with the entire system monitorable via the app's live stream from a camera connected to the control unit. Sensors provide real-time data on temperature, humidity, and oxygen levels. Designed for medical facilities, the system operates within Faraday cages. Management of themes, music, videos, licenses, and users is accessible through a web application.",
     dateRange: "2021 - ongoing",
     techStack: [
       "Python",
@@ -64,15 +73,6 @@ const PROJECTS: ProjectItemProps[] = [
     roleInProject: "Tech Leader",
     company: "Estimity",
   },
-  {
-    title: "LegalSpot",
-    description:
-      "Web application for lawyers to manage their tasks, documents, events, registers and more. The architecture of system was designed to be safe and secure. Each company has its own database and instance of services, all data is separated. App supports workflow of lawyers helping with task tracking and generating documents based on templates created in custom editor.",
-    dateRange: "2022 - ongoing",
-    techStack: ["Python", "TypeScript", "Next.js", "PostgreSQL"],
-    roleInProject: "Architect, Team Leader, Backend Developer",
-    company: "Estimity",
-  },
 ];
 
 export const Projects: FC = () => {
@@ -81,9 +81,9 @@ export const Projects: FC = () => {
       {PROJECTS.map((project) => (
         <ProjectItemCard key={project.title} {...project} />
       ))}
-      <p>
-        I have been involved in more projects where my role was to support less
-        experienced developers.
+      <p className="min-w-[300px] max-w-[900px]">
+        I have been actively involved in multiple projects where my primary role
+        was to support and mentor less experienced developers.
       </p>
     </div>
   );
